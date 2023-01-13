e7cf504979d856171342a3dbe77b5d9ce1061345
zhemdbs <zhemdbs@gmail.com>
2023-01-13 18:22:49 +0900

    userGetCookie2 update

--- cookie_library/js/cookie6.js
+++ cookie_library/js/cookie6.js
@@ -13,3 +13,18 @@ const oneGetCookie = (cname) => {
   return nameValue.length > 0 ? nameValue[1] : 'nothing';
 };
 console.log(oneGetCookie('userid'));
+
+//[10]forEach 메서드를 이용해 userGetCookie2함수 만들기
+console.log(document.cookie); //catid=catman; userid=superman; username=batman; cname=antman
+
+const userGetCookie2 = (cname) => {
+  //1. 객체 변수 선언
+  let cookie = {}; //{catid="catman"; userid="superman"; username="batman"; cname="antman"} 이렇게 저장시키기 위해
+
+  //2. 반복처리 - forEach()
+  document.cookie.split('; ').forEach((el) => {
+    //할일처리
+    console.log(el);
+  });
+};
+console.log('userGetCookie 함수로 리턴된 값 = ' + userGetCookie2('userid'));
