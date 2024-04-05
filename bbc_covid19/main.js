bbc23156fc52b2b759ec300757cd1f6aa1862fa6
zhemdbs <zhemdbs@gmail.com>
2024-04-05 18:02:22 +0900

    bbc_covid19 기능 wip

---
+++ bbc_covid19/main.js
@@ -0,0 +1,32 @@
+(()=>{
+  const graphicWrap = document.querySelector('.scroll__graphic');
+  const graphicItem = document.querySelectorAll('.graphic__item');
+  const bubbleWrap = document.querySelector('.scroll__bubble');
+  const bubbleStep = document.querySelectorAll('.bubble__step');
+  let currentItem;
+
+  for(let i = 0; i < graphicItem.length; i++) {
+    graphicItem[i].dataset.index = i
+    for(let j = 0; j < bubbleStep.length; j++){
+      bubbleStep[j].dataset.index = j
+    }
+
+    window.addEventListener('scroll', ()=>{
+      let step;
+
+      for(let i = 0; i<bubbleStep.length; i++) {
+        step = bubbleStep[i];
+        boundRect = step.getBoundingClientRect();
+
+        if(boundRect.top > window.innerHeight * 0.1 && boundRect.top < window.innerHeight * 0.8) {
+          console.log(currentItem);
+          if(currentItem) {
+            currentItem.classList.remove('visible');
+          }
+          currentItem = graphicItem[step.dataset.index];
+          currentItem.classList.add('visible');
+        }
+      }
+    })
+  }
+})();
