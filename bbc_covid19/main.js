95f236645c6bcd7556b166158ec9e7e2e4df23c7
zhemdbs <zhemdbs@gmail.com>
2024-04-08 17:57:07 +0900

    bbc_covid19 분할 및 class 여부 WIP

--- bbc_covid19/main.js
+++ bbc_covid19/main.js
@@ -1,5 +1,5 @@
 (()=>{
-  const graphicWrap = document.querySelector('.scroll__graphic');
+  const graphicWrap = document.querySelectorAll('.graphic__item-wrap');
   const graphicItem = document.querySelectorAll('.graphic__item');
   const bubbleWrap = document.querySelector('.scroll__bubble');
   const bubbleStep = document.querySelectorAll('.bubble__step');
@@ -10,21 +10,29 @@
     for(let j = 0; j < bubbleStep.length; j++){
       bubbleStep[j].dataset.index = j
     }
-
+    
     window.addEventListener('scroll', ()=>{
       let step;
-
+      let boundRect;
+      
       for(let i = 0; i<bubbleStep.length; i++) {
         step = bubbleStep[i];
         boundRect = step.getBoundingClientRect();
-
+        
         if(boundRect.top > window.innerHeight * 0.1 && boundRect.top < window.innerHeight * 0.8) {
-          console.log(currentItem);
           if(currentItem) {
             currentItem.classList.remove('visible');
           }
           currentItem = graphicItem[step.dataset.index];
           currentItem.classList.add('visible');
+
+          graphicWrap.forEach((item)=>{
+            console.log(currentItem.parentElement === item);
+            item.classList.remove('visible')
+            if(currentItem.parentElement === item) {
+              item.classList.add('visible')
+            }
+          })
         }
       }
     })
